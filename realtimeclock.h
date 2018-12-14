d3b17c0cddde913fde64dd72edffd84ee891b32f
RazorrKitty <razorkitty@null.net>
2018-12-14 17:04:13 +0000

    added real time clock driver

--- realtimeclock.h
+++ realtimeclock.h
@@ -8,16 +8,16 @@
 #ifndef REALTIMECLOCK_H
 #define	REALTIMECLOCK_H
 
-#define i_o   RB4                      //1302I_O           
-#define sclk  RB0                      //1302 clock        
-#define rst   RB5                      //1302 enable bit   
+#define i_o     RB4                      //1302I_O           
+#define sclk    RB0                      //1302 clock        
+#define rst     RB5                      //1302 enable bit   
  
 
  unsigned char time_rx;
 
 void ds1302_init();                             //DS1302 initilize subroutine.
-void set_time();                                //set time subroutine.
-void get_time();                                //get time subroutine.
+void set_time(char *table);                                //set time subroutine.
+void get_time(char *table);                                //get time subroutine.
 void time_write_byte(unsigned char time_tx);    //write one byte subroutine.
 unsigned char  time_read_byte();                //read one byte subroutine.
 
