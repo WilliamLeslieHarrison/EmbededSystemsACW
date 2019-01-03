9defdcf34ffd399ffbf17a08b28cc212852e9f5f
RazorrKitty <razorkitty@null.net>
2019-01-03 21:31:43 +0000

    added conviniance funtion to main

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include "LCD.h"//Declaring the header here
 #include "KeyMatrix.h"
 #include "Thermometer.h"
-//#include "realtimeclock.h"
+#include "realtimeclock.h"
 
 //begin config
 #pragma config FOSC = HS // Oscillator Selection bits (HS oscillator)
@@ -22,6 +22,16 @@ Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for
 programming)*/
 //end config
 
+void get_date_time_string(char* string);
+
+
 void main() {               
     for(;;);
 }                      
+
+void get_date_time(int* date_time) {
+    unsigned char time[7];
+    RealTimeClock_get_time(time);
+    date_time[0] = ((int)((time[0] & 0b01110000) >> 4) * 10) + (int)(time[0] & 0b00001111);
+    date_time[1] = ((int)((time[1] & 0b01110000) >> 4) * 10) + (int)(time[1] & 0b00001111);
+}
