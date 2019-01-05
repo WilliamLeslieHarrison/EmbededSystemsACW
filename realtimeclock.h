3bee694ba6db80ee1b46f5f90f3b33e76023f0ec
RazorKitty <RazorKitty@users.noreply.github.com>
2019-01-05 17:42:46 +0000

    Update realtimeclock.h
    extended a bit

--- realtimeclock.h
+++ realtimeclock.h
@@ -14,13 +14,42 @@
 #define i_o     RB4
 #define rst     RB5
 
-unsigned char RealTimeClock_buff[7];
+int __RealTimeClock_buff[7];
+
+
+int __hex_to_int(unsigned char hex);
+unsigned char __int_to_hex(int integer);
+
 
 void RealTimeClock_write_byte(unsigned char time_tx);
-void RealTimeClock_set_time(unsigned char* time);
+void RealTimeClock_set_burst_time(int* time);
 unsigned char RealTimeClock_read_byte(void);
-unsigned char* RealTimeClock_get_time(void);
+int* RealTimeClock_get_burst_time(void);
+
 void RealTimeClock_init(void);
 
-#endif	/* REALTIMECLOCK_H */
+int RealTimeClock_get_seconds(void);
+void RealTimeClock_set_seconds(int seconds);
 
+int RealTimeClock_get_minutes(void);
+int RealTimeClock_set_minutes(int minutes);
+
+int RealTimeClock_get_hours(void);
+void RealTimeClock_set_hours(int hours);
+
+int RealTimeClock_get_day_of_month(void);
+void RealTimeClock_set_day_of_month(int day);
+
+int RealTimeClock_get_month(void);
+void RealTimeClock_set_month(int month);
+
+int RealTimeClock_get_day_of_week(void);
+void RealTimeClock_set_day_of_week(int day_of_week);
+
+int RealTimeClock_get_year(void);
+void RealTimeClock_set_year(int year);
+
+
+
+
+#endif	/* REALTIMECLOCK_H */
